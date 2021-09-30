2cb7ba5cc463533af894c6ed848918fef98f87fa
mrpinkpigg <aaronknestaut@gmail.com>
2021-09-30 13:17:19 -0400

    added working contact form that doesnt look pretty

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,16 @@
 
-(function () {emailjs.init('YOUR_USER_ID');})();
+(function () {emailjs.init('user_SRFsHIzQTJqJS7BPPvURx');})();
+
+function sendmail() {
+    let fullName = document.getElementById("name").value;
+    let userEmail = document.getElementById("email").value;
+    let userMessage = document.getElementById("message").value;
+
+        var contactParams = {
+            from_name: fullName,
+            from_email: userEmail,
+            message: userMessage
+        };
+
+        emailjs.send('service_pgjn5ea', 'template_u5ve16z', contactParams).then(function (res) {})
+}
